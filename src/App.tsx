73f04ec2fe66e21c7511bcca19444ee6e2dca24f
thomasBelray <thomas.sepulveda.belray@gmail.com>
2021-11-24 10:09:12 -0300

    add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
+import "./App.css";
+
+const ALL_CONTACTS = gql`
+  query {
+    allContacts {
+      id
+      username
+      tel
+    }
+  }
+`;
+
+const CREATE_CONTACT = gql`
+  mutation crearContacto($username: String!, $tel: String!) {
+    addContact(username: $username, tel: $tel) {
+      username
+    }
+  }
+`;
+
+interface Contact {
+  username: String;
+  tel: String;
+  id: String;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const resul = useQuery(ALL_CONTACTS);
+  const [createContact] = useMutation(CREATE_CONTACT, {
+    refetchQueries: [{ query: ALL_CONTACTS }],
+  });
+  const { data, error, loading } = resul;
+
+  const newContact = {
+    username: "Nuevo Contacto",
+    tel: "11052066",
+  };
+  const { username, tel } = newContact;
+
+  const handleClick = () => {
+    createContact({ variables: { username, tel } });
+  };
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <span>{error}</span>;
+  else {
+    return (
+      <>
+        <div className="flex">
+          <h1>Mis contactos</h1>
+          <button className="añadir" onClick={handleClick}>
+            + Añadir
+          </button>
+        </div>
+        <hr />
+        <div className="flex">
+          {data.allContacts.map((contact: Contact) => (
+            <div className="card" key={contact.id.toString()}>
+              <h2>{contact.username}</h2>
+              <p>{contact.tel}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
